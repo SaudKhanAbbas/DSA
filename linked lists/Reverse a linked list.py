f4ad8bf4b0ee6d976c877e28e35567634f1137b6
Saud <saudkhanabbas@gmail.com>
2026-06-30 14:28:24 +0530

    Implement function to reverse a linked list

---
+++ linked lists/Reverse a linked list.py
@@ -0,0 +1,14 @@
+# 7. reverse a linked list 
+def reverseLinkedList(head):
+
+    prev = None
+    curr = head
+
+    while curr:
+
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+    return prev
