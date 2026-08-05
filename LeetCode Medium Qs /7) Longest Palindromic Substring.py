693f90d228ebf4341b68d3c10f1d5f7bf85980b2
Saud <saudkhanabbas@gmail.com>
2026-08-05 11:54:37 +0530

    Add longestPalindrome function for substring search
    Implement function to find the longest palindromic substring in a given string.

---
+++ LeetCode Medium Qs /7) Longest Palindromic Substring.py
@@ -0,0 +1,16 @@
+def longestPalindrome(s: str) -> str:
+    def expand(l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return l + 1, r
+
+    start, end = 0, 1
+    for i in range(len(s)):
+        for l, r in (expand(i, i), expand(i, i + 1)):
+            if r - l > end - start:
+                start, end = l, r
+    return s[start:end]
+
+# Time Complexity: O(n²)
+# Space Complexity: O(1)
