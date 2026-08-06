139d54e1160e7af5216427b6540c8a31b610827a
Saud <saudkhanabbas@gmail.com>
2026-08-06 12:08:01 +0530

    Add solution for dividing players into equal skill teams
    Implement a solution to divide players into teams based on their skill levels, ensuring equal skill sums for each team.

---
+++ LeetCode Medium Qs /11) Divide Players Into Teams of Equal Skill.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        l, r = 0, len(skill) - 1
+        target = skill[l] + skill[r]
+        ans = 0
+
+        while l < r:
+            if skill[l] + skill[r] != target:
+                return -1
+            ans += skill[l] * skill[r]
+            l += 1
+            r -= 1
+
+        return ans
+
+# Pattern: Sorting + Two Pointers
+# Time Complexity: O(n log n)
+# Space Complexity: O(1)
