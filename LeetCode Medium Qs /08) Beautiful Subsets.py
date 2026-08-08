ae23e529fa70023a094480569f1ef7842427a4b4
Saud <saudkhanabbas@gmail.com>
2026-08-08 14:58:48 +0530

    Add comments for problem statement and complexity
    Added problem statement and complexity analysis comments.

--- LeetCode Medium Qs /08) Beautiful Subsets.py
+++ LeetCode Medium Qs /08) Beautiful Subsets.py
@@ -1,3 +1,5 @@
+# LeetCode 2597. The Number of Beautiful Subsets
+
 from collections import defaultdict
 
 class Solution:
@@ -20,5 +22,12 @@ class Solution:
 
         return dfs(0) - 1
 
-# Time Complexity: O(2^n)
-# Space Complexity: O(n)
+
+# Pattern:
+# Backtracking / DFS + Frequency Hash Map
+
+# Time Complexity:
+# O(2^n)
+
+# Space Complexity:
+# O(n)
