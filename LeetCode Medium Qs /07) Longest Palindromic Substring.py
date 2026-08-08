3d4723b664a9c91404de470d0794a0534674c37f
Saud <saudkhanabbas@gmail.com>
2026-08-08 14:58:30 +0530

    Enhance comments in Longest Palindromic Substring
    Added comments explaining the algorithm and its complexities.

--- LeetCode Medium Qs /07) Longest Palindromic Substring.py
+++ LeetCode Medium Qs /07) Longest Palindromic Substring.py
@@ -1,3 +1,5 @@
+# LeetCode 5. Longest Palindromic Substring
+
 def longestPalindrome(s: str) -> str:
     def expand(l, r):
         while l >= 0 and r < len(s) and s[l] == s[r]:
@@ -6,11 +8,20 @@ def longestPalindrome(s: str) -> str:
         return l + 1, r
 
     start, end = 0, 1
+
     for i in range(len(s)):
         for l, r in (expand(i, i), expand(i, i + 1)):
             if r - l > end - start:
                 start, end = l, r
+
     return s[start:end]
 
-# Time Complexity: O(n²)
-# Space Complexity: O(1)
+
+# Pattern:
+# Expand Around Center
+
+# Time Complexity:
+# O(n²)
+
+# Space Complexity:
+# O(1)
